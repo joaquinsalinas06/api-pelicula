6e20e04a8d898103033de54c5c84b1910e110a70
joaquinsalinas06 <103973127+joaquinsalinas06@users.noreply.github.com>
2024-11-07 08:38:21 -0500

    Add files via upload

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -3,25 +3,32 @@ import uuid
 import os
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
-    nombre_tabla = os.environ["TABLE_NAME"]
-    # Proceso
-    uuidv4 = str(uuid.uuid4())
-    pelicula = {
-        'tenant_id': tenant_id,
-        'uuid': uuidv4,
-        'pelicula_datos': pelicula_datos
-    }
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=pelicula)
-    # Salida (json)
-    print(pelicula) # Log json en CloudWatch
-    return {
-        'statusCode': 200,
-        'pelicula': pelicula,
-        'response': response
-    }
+    try:
+        # Entrada (json)
+        print({"tipo": "INFO", "log_datos": event})
+        tenant_id = event['body']['tenant_id']
+        pelicula_datos = event['body']['pelicula_datos']
+        nombre_tabla = os.environ["TABLE_NAME"]
+        # Proceso
+        uuidv4 = str(uuid.uuid4())
+        pelicula = {
+            'tenant_id': tenant_id,
+            'uuid': uuidv4,
+            'pelicula_datos': pelicula_datos
+        }
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(nombre_tabla)
+        response = table.put_item(Item=pelicula)
+        # Salida (json)
+        print({"tipo": "INFO", "log_datos": pelicula})
+        return {
+            'statusCode': 200,
+            'pelicula': pelicula,
+            'response': response
+        }
+    except Exception as e:
+        print({"tipo": "ERROR", "log_datos": {"error": str(e)}})
+        return {
+            'statusCode': 500,
+            'error': str(e)
+        }
